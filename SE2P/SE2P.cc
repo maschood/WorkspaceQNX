957b5a6354b9cae72e524a4108cc8ecb9a81a1fe
Maschood Ahmad <maschhood.ahmad@haw-hamburg.de>
2013-04-04 13:38:01 +0200

    test

--- SE2P/SE2P.cc
+++ SE2P/SE2P.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
+	std::cout << " Welcome to the QNX Momentics IDE" << std::endl;
 	return EXIT_SUCCESS;
 }
+
