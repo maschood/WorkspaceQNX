80838ef8f33f072a13b0c654258c78848cf2439d
Maschood Ahmad <maschhood.ahmad@haw-hamburg.de>
2013-04-16 10:39:03 +0200

    Implementierung serielle schnittstelle

---
+++ SE2P/RS232Test.h
@@ -0,0 +1,21 @@
+/*
+ * RS232Test.h
+ *
+ *  Created on: 16.04.2013
+ *      Author: maschood
+ */
+
+#ifndef RS232TEST_H_
+#define RS232TEST_H_
+
+#include "RS232_1.h"
+#include "RS232_2.h"
+#include <iostream>
+
+class RS232Test {
+public:
+	RS232Test();
+	virtual ~RS232Test();
+};
+
+#endif /* RS232TEST_H_ */
