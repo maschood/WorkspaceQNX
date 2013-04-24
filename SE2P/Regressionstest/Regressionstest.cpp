d28e4aac3198ef6f8806d117f80ebcbd697ba2d5
Nilüfer G <niluefer.guengoer@haw-hamburg.de>
2013-04-24 15:55:50 +0200

    Regressionstest

---
+++ SE2P/Regressionstest/Regressionstest.cpp
@@ -0,0 +1,17 @@
+/*
+ * Regressionstest.cpp
+ *
+ *  Created on: 24.04.2013
+ *      Author: NG
+ */
+
+#include "Regressionstest.h"
+
+Regressionstest::Regressionstest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Regressionstest::~Regressionstest() {
+	// TODO Auto-generated destructor stub
+}
