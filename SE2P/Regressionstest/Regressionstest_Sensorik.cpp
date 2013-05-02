c85ae1a385ecfae0aba2682baafa9e0f38536cf8
Nilüfer G <niluefer.guengoer@haw-hamburg.de>
2013-05-02 21:59:28 +0200

    Regressionstest v3

---
+++ SE2P/Regressionstest/Regressionstest_Sensorik.cpp
@@ -0,0 +1,64 @@
+/*
+ * Regressionstest.cpp
+ *
+ *  Created on: 24.04.2013
+ *      Author: NG
+ */
+
+#include "Regressionstest.h"
+#include "Addresses.h"
+#include "HAL_A/HAL_A.h"
+#include "HAL_S/HAL_S.h"
+#include <stdlib.h>
+
+#define PRINTBOOL(x) x ? "true" : "false"
+
+using namespace std;
+Regressionstest::Regressionstest() {
+}
+
+Regressionstest::~Regressionstest() {
+}
+
+HAL_A* hal_a = HAL_A::get_instance();
+bool erg = false;
+
+bool Regressionstest::test_check_entrance(uint8_t val) {
+
+}
+bool Regressionstest::test_check_hight_determinism(uint8_t val) {
+
+}
+bool Regressionstest::test_hight_mesure() {
+
+}
+bool Regressionstest::test_check_bar(uint8_t val) {
+
+}
+bool Regressionstest::test_check_metal() {
+
+}
+bool Regressionstest::test_check_bar_open() {
+
+}
+bool Regressionstest::test_check_slide_full(uint8_t val) {
+
+}
+bool Regressionstest::test_check_exit(uint8_t val) {
+
+}
+bool Regressionstest::test_check_start_button(uint8_t val) {
+
+}
+bool Regressionstest::test_check_stop_button(uint8_t val) {
+
+}
+bool Regressionstest::test_check_reset_button(uint8_t val) {
+
+}
+bool Regressionstest::test_check_quick_stop(uint8_t val) {
+
+void Regressionstest::TestSensorik() {
+
+	}
+
