a1336f9697247120227b30b10fe88db4b4569523
Nilüfer G <niluefer.guengoer@haw-hamburg.de>
2013-05-04 16:23:50 +0200

    Branch test

--- SE2P/Regressionstest/Regressionstest.h
+++ SE2P/Regressionstest/Regressionstest.h
@@ -16,31 +16,30 @@ public:
 	Regressionstest();
 	virtual ~Regressionstest();
 	bool test_lamp_red_on();
-	void startTest();
+	void TestAktorik();
+	void TestSensorik();
+	void TestLauf();
 private:
 
+	//Aktorik
 	bool test_lamp_yellow_on();
 	bool test_lamp_green_on();
 	bool test_lamp_red_off();
 	bool test_lamp_yellow_off();
 	bool test_lamp_green_off();
 
-	// Schrankensteuerung
 	bool test_bar_open();
 	bool test_bar_close();
 
-	// Flieﬂbandsteuerung
 	bool test_conveyor_move_right();
 	bool test_conveyor_move_left();
 	bool test_conveyor_stop();
 
-	//LED
 	bool test_led_start_button_on();
 	bool test_led_reset_button_on();
 	bool test_led_start_button_off();
 	bool test_led_reset_button_off();
 	bool test_general_switch_off();
-
 };
 
 #endif /* REGRESSIONSTEST_H_ */
