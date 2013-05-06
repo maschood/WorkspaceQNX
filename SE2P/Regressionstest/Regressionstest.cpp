ad36036902573efc7cb15c751bfdb40c8026cf41
Nilüfer G <niluefer.guengoer@haw-hamburg.de>
2013-05-06 15:14:56 +0200

    Regressionstest Stand 6.5.2013

--- SE2P/Regressionstest/Regressionstest.cpp
+++ SE2P/Regressionstest/Regressionstest.cpp
@@ -12,42 +12,59 @@
 #include <stdlib.h>
 
 Regressionstest::Regressionstest() {
-
 }
 
 Regressionstest::~Regressionstest() {
-
 }
 
+bool test = true;
+
 void Regressionstest::TestLauf() {
+
 	HAL_A* hal_a = HAL_A::get_instance();
 
 	uint8_t val = in8(DIO_BASE + DIO_OFFS_B);
 
-	while (true) {
+	while (test) {
 
 		val = in8(DIO_BASE + DIO_OFFS_B);
 
 		if ((~val & BIT_0) == BIT_0) { //Lichtschranke unterbrochen
+			hal_a->lamp_green_on();
 			hal_a->conveyor_move_right();
-		}
-
-		else if ((~val & BIT_1) == BIT_1) { //Höhenmessung
 			sleep(1);
-			hal_a->conveyor_move_right();
+			hal_a->lamp_green_off();
+
 		}
 
-		else if ((~val & BIT_3) == BIT_3) { //Puk an Weiche angekommen
+		if ((~val & BIT_1) == BIT_1) { //Höhenmessung
+			//	sleep(1);
+			hal_a->lamp_red_on();
+			sleep(1);
+			hal_a->lamp_red_off();
+			//hal_a->conveyor_move_right();
+		}
+		if ((~val & BIT_3) == BIT_3) { //Puk an Weiche angekommen
 			hal_a->bar_open();
 			sleep(1);
-			hal_a->conveyor_move_right();
-			sleep(3);
 			hal_a->bar_close();
-			hal_a->conveyor_move_right();
-
-		} else if ((~val & BIT_7) == BIT_7) { //Puk am Ende von Band
+		}
+		if ((~val & BIT_7) == BIT_7) { //Puk am Ende von Band
 			hal_a->conveyor_stop();
+			hal_a->lamp_yellow_on();
+			sleep(1);
+			hal_a->lamp_yellow_off();
+			hal_a->conveyor_move_left();
+			hal_a->bar_open();
+			sleep(2);
+			hal_a->bar_close();
+			hal_a->conveyor_stop();
+			hal_a->conveyor_move_right();
+			sleep(1);
+			hal_a->conveyor_stop();
+			//test = false;
 		}
 
 	}
+
 }
